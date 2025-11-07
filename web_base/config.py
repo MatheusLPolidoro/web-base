bd0450c708bbf4217ee3b0265ad625e74e57ebde
MatheusLPolidoro <matheus.polidoro@fisglobal.com>
2025-11-07 11:19:39 -0300

    [FIX] Ajuste para abertura de Edge sem interface grafica.

--- web_base/config.py
+++ web_base/config.py
@@ -66,7 +66,10 @@ class WebBase:
             options.use_chromium = True
             if self.hidden:
                 options.add_argument('--headless=new')
-            options.add_argument('--start-maximized')
+                options.add_argument('--disable-gpu')
+                options.add_argument('--no-sandbox')
+                options.add_argument('--disable-dev-shm-usage')
+                options.add_argument('--window-size=1920,1080')
             capabilities = DesiredCapabilities.EDGE.copy()
 
         else:
@@ -87,7 +90,9 @@ class WebBase:
             elif self.browser == 'Edge':
                 self.driver = webdriver.Edge(options=options)
 
-            self.driver.maximize_window()
+            if not self.hidden:
+                self.driver.maximize_window()
+
             self.status = True
 
         except Exception as ex:
